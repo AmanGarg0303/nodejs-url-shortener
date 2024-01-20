7c559892a4c2869d8983b39c217c71ae46ca6a41
Aman <amangarg0303@gmail.com>
2024-01-20 22:38:25 +0530

    SSR using ejs

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import urlRoutes from "./routes/url.js";
 const app = express();
 const PORT = 8000;
 
+app.set("view engine", "ejs");
+
 connectDb("mongodb://127.0.0.1:27017/short-url")
   .then(() => console.log("Mongo connected."))
   .catch((err) => console.log("Mongo error!"));
 
 app.use(express.json());
+
+app.use("/test", (req, res) => {
+  return res.render("home");
+});
 
 app.use("/url", urlRoutes);
 
